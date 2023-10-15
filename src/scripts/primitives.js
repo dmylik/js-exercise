e1aee4279c3fb8d194356f44b05b39e8e3b2e8e6
dmylik <dzmitry.mialik@gmail.com>
2023-10-15 21:46:14 +0300

    1. Переменные

---
+++ src/scripts/primitives.js
@@ -0,0 +1,42 @@
+let thisISLet;
+// const thisIsConst;
+const thisIsConst = 0; //блочные
+var thisIsVar; //функциональные
+let user = 'John', age = 25, message = 'Hello';
+
+// let thisISLet;
+// var thisIsVar;
+// const thisIsConst;
+
+{
+    let thisISLet;
+    let letInBlock = "letInBlock";
+    var varInBlock = "varInBlock";
+}
+
+// if(1)
+//     let letInIf = "letInIf";
+// else
+//     const constInElse  = 'constInElse';
+
+// console.log(letInIf);
+
+// function block(){
+//     var start = "start";
+// }
+
+// console.log(start)
+
+
+// console.log(letInBlock)
+// console.log(varInBlock)
+
+// (function() {
+//     var IIFE = "Hello";
+//     console.log(IIFE); // Hello
+// })();
+//
+// console.log(IIFE); // Hello
+
+
+
