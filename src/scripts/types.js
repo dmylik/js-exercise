5955cde91fa483eebcf1c3772a3b709dc2682073
dmylik <dzmitry.mialik@gmail.com>
2023-10-17 21:37:35 +0300

    2. Условия

--- src/scripts/types.js
+++ src/scripts/types.js
@@ -1,85 +1,129 @@
-// console.log("6" / "2");
-// console.log( '1' + 2 );
-// console.log(2 + 2 + '1' );
-// console.log( 6 - '2' );
-// console.log( +true );
-// console.log( +"" );
+function wr(){
+    console.log(...arguments)
+}
+
+// wr("6" / "2");
+// wr( '1' + 2 );
+// wr(2 + 2 + '1' );
+// wr( 6 - '2' );
+// wr( +true );
+// wr( +"" );
 //
 // let a, b, c;
 //
 // a = b = c = 2 + 2;
 //
-// console.log( a ); // 4
-// console.log( b ); // 4
-// console.log( c ); // 4
+// wr( a ); // 4
+// wr( b ); // 4
+// wr( c ); // 4
 //
-// console.log( 'Я' > 'А' );
-// console.log( 'Коты' > 'Кода' );
-// console.log( 'Сонный' > 'Сон' );
+// wr( 'Я' > 'А' );
+// wr( 'Коты' > 'Кода' );
+// wr( 'Сонный' > 'Сон' );
 
-// console.log( '2' > 1 );
-// console.log( 1 > '2' );
-// console.log( '01' == 1 );
+// wr( '2' > 1 );
+// wr( 1 > '2' );
+// wr( '01' == 1 );
 
 
 
-// console.log(false);
-// console.log(Boolean(''));
-// console.log(Boolean(0));
-// console.log(Boolean(0n));
-// console.log(Boolean(-0));
-// console.log(Boolean(-0n));
-// console.log(Boolean(NaN));
-// console.log(Boolean(undefined));
-// console.log(Boolean(null));
+// wr(false);
+// wr(Boolean(''));
+// wr(Boolean(0));
+// wr(Boolean(0n));
+// wr(Boolean(-0));
+// wr(Boolean(-0n));
+// wr(Boolean(NaN));
+// wr(Boolean(undefined));
+// wr(Boolean(null));
 
-console.log(null === undefined)
 
-if (0) { // 0 is falsy
-}
+//
+// wr(null === undefined)
+//
+// if (0) { // 0 is falsy
+// }
+//
+// let accessAllowed, age=20;
+//
+// if(age>18)
+//     accessAllowed = true;
+// else
+//     accessAllowed = false;
+//
+// let accessAllowed1 = (age > 18) ? true : false;
+// let message;
+//
+// if (age < 3) {
+//     message = 'Здравствуй, малыш!';
+// } else if (age < 18) {
+//     message = 'Привет!';
+// } else if (age < 100) {
+//     message = 'Здравствуйте!';
+// } else {
+//     message = 'Какой необычный возраст!';
+// }
+//
+// // message = (age < 3) ? 'Здравствуй, малыш!' :
+// //     (age < 18) ? 'Привет!' :
+// //         (age < 100) ? 'Здравствуйте!' :
+// //             'Какой необычный возраст!';
+//
+// wr( message );
+//
+// let firstName = "";
+// let lastName = "";
+// let nickName = "Суперкодер";
+//
+// wr( firstName || lastName || nickName || "Аноним"); // Суперкодер
+//
+// wr(NaN || 2 || true || '1')
+// wr(0 || '' || false || undefined)
+// wr(( 1 && 2 && 3 && 4 ));
+// wr(( 1 && 2 && null && 3 ));
+// wr( null || 2 || undefined );
+// wr( null || 2 && 3 || 4 );
+//
+// let value = NaN;
+//
+// value &&= 10;
+// value ||= 20;
+// value &&= 30;
+// value ||= 40;
+//
+// wr(value)
+//
+// let a =  2 + 2;
+//
+// switch (a) {
+//     case 3:
+//         wr( 'Маловато' );
+//         break;
+//     case 4:
+//         wr( 'В точку!' );
+//         // break;
+//     case 5:
+//     case 6:
+//     case 7:
+//         wr( 'Перебор' );
+//         break;
+//     default:
+//         wr( "Нет таких значений" );
+// }
+//
+// const  YourName = 'Максим'
+// switch (YourName) {
+//     case 'Максим':
+//         wr( "Его родители отличные мужчины" );
+//         break;
+//
+//     case 'Дима':
+//     case 'Петя':
+//     case 'Лехя':
+//         wr( 'Красавчики' );
+//         break;
+//
+//     default:
+//         wr( 'Эдик это вообще ппц, а Андрей это диагнрос' );
+// }
 
-let accessAllowed, age=20;
-
-if(age>18)
-    accessAllowed = true;
-else
-    accessAllowed = false;
-
-let accessAllowed1 = (age > 18) ? true : false;
-let message;
-
-if (age < 3) {
-    message = 'Здравствуй, малыш!';
-} else if (age < 18) {
-    message = 'Привет!';
-} else if (age < 100) {
-    message = 'Здравствуйте!';
-} else {
-    message = 'Какой необычный возраст!';
-}
-
-// message = (age < 3) ? 'Здравствуй, малыш!' :
-//     (age < 18) ? 'Привет!' :
-//         (age < 100) ? 'Здравствуйте!' :
-//             'Какой необычный возраст!';
-
-console.log( message );
-
-let firstName = "";
-let lastName = "";
-let nickName = "Суперкодер";
-
-console.log( firstName || lastName || nickName || "Аноним"); // Суперкодер
-
-console.log(( 1 && 2 && null && 3 ));
-console.log(( null || 2 || undefined ));
-console.log( null || 2 && 3 || 4 );
-
-let value = NaN;
-
-value &&= 10;
-value ||= 20;
-value &&= 30;
-value ||= 40;
-
-console.log(value)
